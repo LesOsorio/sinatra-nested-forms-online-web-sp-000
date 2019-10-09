73b1ded6ee3ccccc90b523a7052dfa403436a2c6
Leslie Osorio <lesosorio17@gmail.com>
2019-10-09 16:30:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,15 +11,14 @@ module FormsLab
       erb :'pirates/new'
     end
 
-    post '/pirates' do
+    get '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      #binding.pry
+
       params[:pirate][:ships].each do |details|
         Ship.new(details)
-      #binding.pry
       end
       @ships = Ship.all
-      #binding.pry
+
       erb :'pirates/show'
     end
 
